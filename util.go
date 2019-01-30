38f324416e717209270aba534037fb2b0384692c
zxysilent <zxysilent@foxmail.com>
2019-01-30 09:54:01 +0800

    优化速度 107 ns/op

--- util.go
+++ util.go
@@ -14,15 +14,25 @@ const (
 	FmtyyyyMMdd = "20060102"
 	chars       = "0123456789_abcdefghijkl-mnopqrstuvwxyz" //ABCDEFGHIJKLMNOPQRSTUVWXYZ
 	charsLen    = len(chars)
+	mask        = 1<<6 - 1
 )
 
 var rng = rand.NewSource(time.Now().UnixNano())
 
 // RandStr 返回指定长度的随机字符串
 func RandStr(ln int) string {
+	/* chars 38个字符
+	 * rng.Int63() 每次产出64bit的随机数,每次我们使用6bit(2^6=64) 可以使用10次
+	 */
 	buf := strings.Builder{}
-	for i := 0; i < ln; i++ {
-		buf.WriteByte(chars[rng.Int63()%int64(charsLen)])
+	for idx, cache, remain := ln, rng.Int63(), 10; idx > 0; {
+		if remain == 0 {
+			cache, remain = rng.Int63(), 10
+		}
+		buf.WriteByte(chars[int(cache&mask)%charsLen])
+		cache >>= 6
+		remain--
+		idx--
 	}
 	return buf.String()
 }
