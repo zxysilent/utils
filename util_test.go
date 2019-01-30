eefd34eb08e6b3596f7e51d48afd76a5b58d1c32
zxysilent <zxysilent@foxmail.com>
2019-01-30 09:31:12 +0800

    添加测试

--- util_test.go
+++ util_test.go
@@ -5,7 +5,13 @@ import (
 )
 
 // 性能测试
-func BenchmarkRandStr6(b *testing.B) {
+func BenchmarkRandStr8(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RandStr(8)
+	}
+}
+
+func BenchmarkRandStr16(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		RandStr(16)
 	}
