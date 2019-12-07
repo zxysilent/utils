b24322bc6acd2ffd70ef650c562768c233498c4e
zxysilent <zxysilent@outlook.com>
2019-12-07 13:43:34 +0800

    [A] template

---
+++ tmpl.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+)
+
+// LoadTmpl 加载指定目录解析为模板 仅限 .html 文件
+// eg: views/index.html ,views/subtmpl/subtmpl.html
+// tmpl:=LoadTmpl("./views")
+// tmpl.ExecuteTemplate(yourWriter, "index.html", yourData)、tmpl.ExecuteTemplate(yourWriter, "subtmpl/subtmpl.html", yourData)
+func LoadTmpl(root string) *template.Template {
+	tmpl := template.New("LoadTmpl")
+	rln := len(root)
+	filepath.Walk(root, func(path string, fi os.FileInfo, err error) error {
+		pln := len(path)
+		// 是文件 并且是 .html 结尾
+		if !fi.IsDir() && pln > 4 && path[pln-5:] == ".html" {
+			buf, err := ioutil.ReadFile(path)
+			if err != nil {
+				panic("read file :" + path + ". error :" + err.Error())
+			}
+			name := strings.ReplaceAll(path, "\\", "/")
+			_, err = tmpl.New(name[rln-1:]).Parse(string(buf))
+			if err != nil {
+				panic("parse file :" + path + ". error :" + err.Error())
+			}
+		}
+		return nil
+	})
+	return tmpl
+}
---
+++ tmpl_test.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestLoadTmpl(t *testing.T) {
+	tmpl := LoadTmpl("./testdata/views")
+	if tmpl == nil {
+		t.Error("error")
+	}
+	tmpls := tmpl.Templates()
+	for i := 0; i < len(tmpls); i++ {
+		t.Log(tmpls[i].Name())
+	}
+}
+
+func TestLoadTmplExec(t *testing.T) {
+	mod := struct {
+		Name string
+		Arr  []int
+	}{
+		Name: "testName",
+		Arr:  []int{1, 3, 5, 7},
+	}
+	tmpl := LoadTmpl("./testdata/views")
+	if tmpl == nil {
+		t.Error("error")
+	}
+	w := bytes.NewBuffer(nil)
+	tmpl.ExecuteTemplate(w, "subtmpl/subtmpl.html", mod)
+	t.Log(w.String())
+}
