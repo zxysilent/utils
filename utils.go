98050967bce1583a30b69d8e3c9425ce819bb2b6
zxysilent <zxysilent@outlook.com>
2021-07-05 13:32:35 +0800

    [U] 优化命名

--- utils.go
+++ utils.go
@@ -3,6 +3,8 @@ package utils
 const (
 	// StdDateTime 标准日期格式化
 	StdDateTime = "2006-01-02 15:04:05"
-	// StdyyyyMMdd 年月日
-	StdyyyyMMdd = "20060102"
+	// StdDate 年月日
+	StdDate = "20060102"
+	// StdTime 时分秒
+	StdTime = "15:04:05"
 )
