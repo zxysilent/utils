9c7cf664f262e9c80e67bc9ded7f820e2712dd11
zxysilent <zxysilent@outlook.com>
2021-07-05 13:30:55 +0800

    [U] 文件名-优化命名

--- util.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-const (
-	// FormatDateTime 标准日期格式化
-	FormatDateTime = "2006-01-02 15:04:05"
-	// FmtyyyyMMdd 年月日
-	FmtyyyyMMdd = "20060102"
-)
---
+++ utils.go
@@ -0,0 +1,8 @@
+package utils
+
+const (
+	// StdDateTime 标准日期格式化
+	StdDateTime = "2006-01-02 15:04:05"
+	// StdyyyyMMdd 年月日
+	StdyyyyMMdd = "20060102"
+)
